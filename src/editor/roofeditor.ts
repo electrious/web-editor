f766fa55fc612287aee59266d239b37fc6279da5
Eric Wong <ericsyw@gmail.com>
2019-11-06 21:52:08 +0800

    Move red marker code into standalone functions

--- src/editor/roofeditor.ts
+++ src/editor/roofeditor.ts
@@ -11,20 +11,87 @@ import {
     switchLatest,
     mergeArray,
     snapshot,
-    merge
+    merge,
+    constant
 } from '@most/core'
 import { createDraggableObject, DraggableObject } from '../ui/draggableobject'
 import { mkSink } from '../sink'
 import { defScheduler } from '../helper'
-import { disposeBoth, dispose, disposeAll } from '@most/disposable'
+import { dispose, disposeAll } from '@most/disposable'
 import pluck from 'ramda/es/pluck'
 import remove from 'ramda/es/remove'
+import curry from 'ramda/es/curry'
 
 const toVec2 = (v: Vector3): Vector2 => {
     return new Vector2(v.x, v.y)
 }
 const mkArray = (...args: Vector3[]) => {
     return args.map(toVec2)
+}
+
+/**
+ * create red markers for vertices
+ */
+const mkRedMarkers = curry(
+    (
+        roofActive: Stream<boolean>,
+        activeMarker: Stream<number | null>,
+        ps: Vector2[]
+    ): DraggableObject[] => {
+        const res = ps.map((pos: Vector2, idx: number) => {
+            // determine whether the current marker should be active or not
+            const f = (roofActive: boolean, activeIdx: number | null) => {
+                return roofActive && (activeIdx == null || activeIdx == idx)
+            }
+            const isActive = multicast(combine(f, roofActive, activeMarker))
+
+            // create the marker
+            return createDraggableObject(isActive, idx, pos)
+        })
+        return res
+    }
+)
+
+/**
+ * get red markers' active status event stream
+ * @param ms a stream of all red markers
+ */
+const getRedMarkerActiveStatus = (
+    ms: Stream<DraggableObject[]>
+): Stream<number | null> => {
+    // get active marker when any one is being dragged
+    const g = (m: DraggableObject, idx: number) => {
+        return map(d => {
+            return d ? idx : null
+        }, m.isDragging)
+    }
+
+    const h = (ms: DraggableObject[]) => {
+        return mergeArray(ms.map(g))
+    }
+    const statusForDragging = switchLatest(map(h, ms))
+
+    // set active marker to null when all markers are just created
+    const statusForNewMarker = constant(null, ms)
+
+    return merge(statusForDragging, statusForNewMarker)
+}
+
+// delete old marker objects and add new ones. this will return a function
+// that can be used in 'loop' directly
+const attachObjs = (parent: Object3D) => {
+    return (objs: DraggableObject[], newObjs: DraggableObject[]) => {
+        objs.forEach(o => {
+            parent.remove(o.object)
+            dispose(o.disposable)
+        })
+
+        newObjs.forEach(o => {
+            parent.add(o.object)
+        })
+
+        return { seed: newObjs, value: newObjs }
+    }
 }
 
 export interface RoofEditor {
@@ -38,69 +105,27 @@ export const createRoofEditor = (
     active: Stream<boolean>,
     ps: Vector2[]
 ): RoofEditor => {
-    // internal stream for currently active marker
-    const [setActive, activeMarker] = createAdapter<number | null>()
+    const scheduler = defScheduler()
 
     // internal stream for maintaning the roof active status
     const [setRoofActive, roofActive] = createAdapter<boolean>()
 
-    const scheduler = defScheduler()
-
     // pipe the 'active' param event into internal roofActive stream
     const disposable1 = active.run(mkSink(setRoofActive), scheduler)
 
-    // streams for new list of vertices
+    // stream for new list of vertices
     const [updateVertList, vertices] = createAdapter<Vector2[]>()
 
-    // function to create new markers for a list of vertices
-    const mkMarkers = (ps: Vector2[]): DraggableObject[] => {
-        const res = ps.map((pos: Vector2, idx: number) => {
-            // determine whether the current marker should be active or not
-            const f = (roofActive: boolean, activeIdx: number | null) => {
-                return roofActive && (activeIdx == null || activeIdx == idx)
-            }
-            const isActive = multicast(combine(f, roofActive, activeMarker))
-
-            // create the marker
-            const marker = createDraggableObject(isActive, idx, pos)
-
-            // if the current marker is being dragged, then it's active
-            const g = (dragging: boolean): number | null => {
-                return dragging ? idx : null
-            }
-            const d = map(g, marker.isDragging).run(
-                mkSink(setActive),
-                scheduler
-            )
-
-            marker.disposable = disposeBoth(marker.disposable, d)
-
-            return marker
-        })
-        return res
-    }
-
-    // delete old marker objects and add new ones
-    const attachObjs = (
-        objs: DraggableObject[],
-        newObjs: DraggableObject[]
-    ) => {
-        objs.forEach(o => {
-            parent.remove(o.object)
-            dispose(o.disposable)
-        })
-
-        newObjs.forEach(o => {
-            parent.add(o.object)
-        })
-
-        setActive(null)
-
-        return { seed: newObjs, value: newObjs }
-    }
+    // internal stream for currently active marker
+    const [setActive, activeMarker] = createAdapter<number | null>()
 
     // create new markers and attach them to the parent object.
-    const markers = multicast(loop(attachObjs, [], map(mkMarkers, vertices)))
+    const markerObjs = map(mkRedMarkers(roofActive, activeMarker), vertices)
+    const markers = multicast(loop(attachObjs(parent), [], markerObjs))
+
+    // stream for active red marker
+    const actMarker = getRedMarkerActiveStatus(markers)
+    const disposable3 = actMarker.run(mkSink(setActive), scheduler)
 
     // get new positions after dragging
     const getPosition = (o: DraggableObject[]) => {
@@ -127,6 +152,10 @@ export const createRoofEditor = (
     const disposable2 = vertsAfterDel.run(
         mkSink(vs => {
             updateVertList(vs)
+
+            // reset rofoActive to true so that the 'combine' call in
+            // mkMarkers func will be able to continue without waiting for
+            // explicit event from the 'active' param stream
             setRoofActive(true)
         }),
         scheduler
@@ -138,6 +167,6 @@ export const createRoofEditor = (
     return {
         // skip the first occurrence as it's the same values as provided
         roofVertices: newVertices,
-        disposable: disposeAll([disposable1, disposable2])
+        disposable: disposeAll([disposable1, disposable2, disposable3])
     }
 }
