50ecbe76a95476fe43533ebd5e52913a6f278390
Eric Wong <ericsyw@gmail.com>
2020-04-08 18:03:06 +0800

    Simple change

--- example/index.js
+++ example/index.js
@@ -2,6 +2,7 @@ import { createEditor } from '../src/Editor.purs'
 import { testRoofs } from './testroofplates'
 
 const parent = document.querySelector('#editor')
+const serverUrl = 'https://s3.eu-west-1.amazonaws.com/data.electrious.com'
 
 if (parent != null) {
     // create editor instance
@@ -9,5 +10,5 @@ if (parent != null) {
     // convert test data to RoofPlate objects
     const roofs = testRoofs.roofplates
     // load the house and roofs
-    editor.loadHouse('https://s3.eu-west-1.amazonaws.com/data.electrious.com')(296285)(roofs)(console.log)()
+    editor.loadHouse(serverUrl)(296285)(roofs)(console.log)()
 }
