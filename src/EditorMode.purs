107a6268af1e40569dd03875061f0062a3f26be2
Eric Wong <ericsyw@gmail.com>
2020-05-12 18:02:07 +0800

    Make EditorMode instance of Encode and Decode

--- src/EditorMode.purs
+++ src/EditorMode.purs
@@ -2,11 +2,15 @@ module Editor.EditorMode where
 
 import Prelude
 
-import Data.Enum (class BoundedEnum, class Enum)
+import Control.Monad.Error.Class (throwError)
+import Data.Enum (class BoundedEnum, class Enum, fromEnum, toEnum)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Bounded (genericBottom, genericTop)
 import Data.Generic.Rep.Enum (genericCardinality, genericFromEnum, genericPred, genericSucc, genericToEnum)
 import Data.Generic.Rep.Show (genericShow)
+import Data.List.NonEmpty (singleton)
+import Data.Maybe (Maybe(..))
+import Foreign.Generic (class Decode, class Encode, ForeignError(..), decode, encode)
 
 -- | Define the different modes the editor is running in.
 data EditorMode = Showing
@@ -28,3 +32,10 @@ instance boundEnumEditorMode :: BoundedEnum EditorMode where
     cardinality = genericCardinality
     toEnum = genericToEnum
     fromEnum = genericFromEnum
+instance encodeEditorMode :: Encode EditorMode where
+    encode = fromEnum >>> encode
+instance decodeEditorMode :: Decode EditorMode where
+    decode o = decode o >>= \i -> do
+                case toEnum i of
+                    Just v -> pure v
+                    Nothing -> throwError $ singleton $ ForeignError $ "Can't decode EditorMode from: " <> show i
