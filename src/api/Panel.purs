b4eb0a84825787345b16fef6766c78f93c2cdc8f
Eric Wong <ericsyw@gmail.com>
2020-10-10 11:37:03 +0800

    Delete id field in create panel api request body

--- src/api/Panel.purs
+++ src/api/Panel.purs
@@ -6,15 +6,30 @@ import API (API, callAPI')
 import Axios.Types (Method(..))
 import Data.Lens ((^.))
 import Data.UUID (UUID, toString)
+import Editor.Common.Lenses (_alignment, _orientation, _slope, _x, _y)
 import FRP.Event (Event)
-import Model.Roof.Panel (Panel, _roofUUID)
+import Model.Roof.Panel (Panel, _arrNumber, _roofUUID, _row_number, _uuid)
+
+
+-- delete id field in Panel to be used in create panel APIs
+delPanelId p = {
+    uuid           : p ^. _uuid,
+    roofplate_uuid : p ^. _roofUUID,
+    row_number     : p ^. _row_number,
+    array_number   : p ^. _arrNumber,
+    x              : p ^. _x,
+    y              : p ^. _y,
+    slope          : p ^. _slope,
+    orientation    : p ^. _orientation,
+    alignment      : p ^. _alignment
+}
 
 createPanel :: Int -> Panel -> API (Event Unit)
-createPanel leadId p = callAPI' POST url p
+createPanel leadId p = callAPI' POST url $ delPanelId p
     where url = "/leads/" <> show leadId <> "/roofplates/" <> show (p ^. _roofUUID) <> "/panels"
 
 createPanels :: Int -> UUID -> Array Panel -> API (Event Unit)
-createPanels leadId roofId ps = callAPI' POST url ps
+createPanels leadId roofId ps = callAPI' POST url $ delPanelId <$> ps
     where url = "/leads/" <> show leadId <> "/roofplates/" <> toString roofId <> "/panels"
 
 deletePanels :: Int -> Array UUID -> API (Event Unit)
