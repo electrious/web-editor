da14c5fb9a8b16aa67a673770a4b4dbcdb17d8e9
Eric Wong <ericsyw@gmail.com>
2020-10-23 20:59:05 +0800

    Use FFI instead of ffi/fpi functions to improve performance

--- src/external/Three/Core/Face3.js
+++ src/external/Three/Core/Face3.js
@@ -3,3 +3,19 @@ const three = require('three')
 exports.mkFace3 = a => b => c => _ => {
     return new three.Face3(a, b, c)
 }
+
+exports.indexA = f => {
+    return f.a
+}
+
+exports.indexB = f => {
+    return f.b
+}
+
+exports.indexC = f => {
+    return f.c
+}
+
+exports.normal = f => {
+    return f.normal
+}
--- src/external/Three/Core/Face3.purs
+++ src/external/Three/Core/Face3.purs
@@ -2,20 +2,12 @@ module Three.Core.Face3 where
 
 import Effect (Effect)
 import Three.Math.Vector (Vector3)
-import Util (ffi)
 
 foreign import data Face3 :: Type
 
 foreign import mkFace3 :: Int -> Int -> Int -> Effect Face3
 
-indexA :: Face3 -> Int
-indexA = ffi ["face"] "face.a"
-
-indexB :: Face3 -> Int
-indexB = ffi ["face"] "face.b"
-
-indexC :: Face3 -> Int
-indexC = ffi ["face"] "face.c"
-
-normal :: Face3 -> Vector3
-normal = ffi ["face"] "face.normal"
+foreign import indexA :: Face3 -> Int
+foreign import indexB :: Face3 -> Int
+foreign import indexC :: Face3 -> Int
+foreign import normal :: Face3 -> Vector3
