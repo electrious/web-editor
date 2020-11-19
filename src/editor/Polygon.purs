3648d6db896133200918afd427b4e7bd246349ca
Eric Wong <eric.wong@clean.tech>
2020-11-19 14:56:24 +0000

    Add renderPolygon function

--- src/editor/Polygon.purs
+++ src/editor/Polygon.purs
@@ -1,8 +1,25 @@
 module Editor.Polygon where
 
+import Prelude
+
+import Custom.Mesh (TappableMesh, mkTappableMesh)
+import Data.Lens ((^.), Lens')
+import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Newtype (class Newtype)
+import Effect (Effect)
+import Three.Core.Geometry (mkShape, mkShapeGeometry)
+import Three.Core.Material (MeshBasicMaterial)
 import Three.Math.Vector (Vector2)
 
 newtype Polygon = Polygon (Array Vector2)
 
 derive instance newtypePolygon :: Newtype Polygon _
+
+_polyVerts :: Lens' Polygon (Array Vector2)
+_polyVerts = _Newtype
+
+renderPolygon :: Polygon -> MeshBasicMaterial -> Effect TappableMesh
+renderPolygon p mat = do
+    shp <- mkShape $ p ^. _polyVerts
+    geo <- mkShapeGeometry shp
+    mkTappableMesh geo mat
