6eab786be4810cf807ec58adccb4a31beee7cbe2
Eric Wong <ericsyw@gmail.com>
2020-11-24 01:00:06 +0000

    Add render' function in CacheRenderable

--- src/rendering/CacheRenderer.purs
+++ src/rendering/CacheRenderer.purs
@@ -1,20 +1,21 @@
 module Rendering.CacheRenderer where
 
-import Prelude
+import Prelude hiding (add)
 
-import Control.Monad.Reader (class MonadAsk, class MonadReader, ReaderT, ask)
-import Control.Monad.State (class MonadState, StateT, lift)
+import Control.Monad.Reader (class MonadAsk, class MonadReader, ReaderT)
+import Control.Monad.State (class MonadState, StateT, get, lift, put)
 import Data.Default (class Default)
-import Data.Lens (Lens')
+import Data.Lens (Lens', (^.), (%~))
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.List (List(..))
+import Data.List (List(..), head, tail)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Newtype (class Newtype)
 import Data.Symbol (SProxy(..))
-import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Class (class MonadEffect)
 import Rendering.Renderable (class Renderable, RenderingM)
 import Rendering.Renderable as R
-import Three.Core.Object3D (class IsObject3D, add)
+import Three.Core.Object3D (class IsObject3D)
 
 
 newtype Cache a = Cache {
@@ -36,7 +37,7 @@ _idle = _Newtype <<< prop (SProxy :: SProxy "idle")
 class Renderable v n <= ReRenderable v n where
     updateNode :: n -> v -> RenderingM Unit
 
-newtype CacheRenderingM parent c a = CacheRenderingM (StateT (Cache c) (ReaderT parent RenderingM) a)
+newtype CacheRenderingM p c a = CacheRenderingM (StateT (Cache c) (ReaderT p RenderingM) a)
 
 derive newtype instance functorCacheRenderingM     :: Functor (CacheRenderingM p c)
 derive newtype instance applyCacheRenderingM       :: Apply (CacheRenderingM p c)
@@ -51,8 +52,21 @@ derive newtype instance monadEffectCacheRenderingM :: MonadEffect (CacheRenderin
 liftRenderingM :: forall p c a. RenderingM a -> CacheRenderingM p c a
 liftRenderingM r = CacheRenderingM $ lift $ lift r
 
-render :: forall p c cn. IsObject3D p => Renderable c cn => c -> CacheRenderingM p cn Unit
+-- render a value to a new or idle node, internal use only
+render' :: forall p c cn. IsObject3D p => ReRenderable c cn => c -> CacheRenderingM p cn cn
+render' val = do
+    c <- get
+    let idle = c ^. _idle
+    case head idle of
+        Nothing -> do
+            n <- CacheRenderingM $ lift $ lift $ R.render val
+            pure n
+        Just n  -> do
+            liftRenderingM $ updateNode n val
+            let nc = c # _idle %~ tail >>> fromMaybe Nil
+            put nc
+            pure n
+
+render :: forall p c cn. IsObject3D p => ReRenderable c cn => c -> CacheRenderingM p cn Unit
 render val = do
-    n <- liftRenderingM $ R.render val
-    p <- ask
-    liftEffect $ add n p
+    
