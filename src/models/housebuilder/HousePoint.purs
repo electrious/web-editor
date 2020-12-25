47b6af851b72300f79ffe3c965e4c2361efafc9f
Eric Wong <ericsyw@gmail.com>
2020-12-25 17:25:06 +0800

    Redefine RidgePoint, GutterPoint and Ridge data types

--- src/models/housebuilder/HousePoint.purs
+++ src/models/housebuilder/HousePoint.purs
@@ -1,16 +1,29 @@
 module Model.HouseEditor.HousePoint where
 
-import Prelude
 
+import Data.Lens (Lens')
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Newtype (class Newtype)
 import Three.Math.Vector (Vector3)
 
 
-data HousePoint = GutterPoint Vector3
-                | RidgePoint Vector3
+newtype RidgePoint = RidgePoint Vector3
 
-derive instance eqHousePoint :: Eq HousePoint
+derive instance newtypeRidgePoint :: Newtype RidgePoint _
 
-pointPos :: HousePoint -> Vector3
-pointPos (GutterPoint p) = p
-pointPos (RidgePoint p)  = p
+ridgePoint :: Vector3 -> RidgePoint
+ridgePoint = RidgePoint
 
+_ridgePointPos :: Lens' RidgePoint Vector3
+_ridgePointPos = _Newtype
+
+
+newtype GutterPoint = GutterPoint Vector3
+
+derive instance newtypeGutterPoint :: Newtype GutterPoint _
+
+gutterPoint :: Vector3 -> GutterPoint
+gutterPoint = GutterPoint
+
+_gutterPointPos :: Lens' GutterPoint Vector3
+_gutterPointPos = _Newtype
--- src/models/housebuilder/Ridge.purs
+++ src/models/housebuilder/Ridge.purs
@@ -1,50 +1,21 @@
 module Model.HouseBuilder.Ridge where
 
-import Prelude
 
-import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Show (genericShow)
-import Data.Lens (Lens', (^.))
-import Data.Lens.Iso.Newtype (_Newtype)
-import Data.Lens.Record (prop)
-import Data.Newtype (class Newtype)
-import Data.Symbol (SProxy(..))
+import Data.Lens ((^.))
 import Math.Line (Line, mkLine)
-import Model.HouseEditor.HousePoint (HousePoint, pointPos)
-
-data RidgeType = TopRidge   -- Ridge connects two RidgePoints
-               | SideRidge  -- Ridge connects a RidgePoint and a GutterPoint
-               | Gutter     -- Gutter connects two GutterPoints
-
-derive instance genericRidgeType :: Generic RidgeType _
-derive instance eqRidgeType :: Eq RidgeType
-instance showRidgeType :: Show RidgeType where
-    show = genericShow
+import Model.HouseEditor.HousePoint (GutterPoint, RidgePoint, _gutterPointPos, _ridgePointPos)
 
 
-newtype Ridge = Ridge {
-    ridgeType :: RidgeType,
-    point1    :: HousePoint,
-    point2    :: HousePoint
-}
+data Ridge = TopRidge  RidgePoint RidgePoint
+           | SideRidge RidgePoint GutterPoint
 
-derive instance genericRidge :: Generic Ridge _
-derive instance newtypeRidge :: Newtype Ridge _
-derive instance eqRidge :: Eq Ridge
 
-_ridgeType :: forall t a r. Newtype t { ridgeType :: a | r } => Lens' t a
-_ridgeType = _Newtype <<< prop (SProxy :: SProxy "ridgeType")
+topRidge :: RidgePoint -> RidgePoint -> Ridge
+topRidge p1 p2 = TopRidge p1 p2
 
-_point1 :: forall t a r. Newtype t { point1 :: a | r } => Lens' t a
-_point1 = _Newtype <<< prop (SProxy :: SProxy "point1")
-
-_point2 :: forall t a r. Newtype t { point2 :: a | r } => Lens' t a
-_point2 = _Newtype <<< prop (SProxy :: SProxy "point2")
-
-mkRidge :: RidgeType -> HousePoint -> HousePoint -> Ridge
-mkRidge t p1 p2 = Ridge { ridgeType : t, point1 : p1, point2 : p2 }
+sideRidge :: RidgePoint -> GutterPoint -> Ridge
+sideRidge p1 p2 = SideRidge p1 p2
 
 ridgeLine :: Ridge -> Line
-ridgeLine r = mkLine v1 v2
-    where v1 = pointPos $ r ^. _point1
-          v2 = pointPos $ r ^. _point2
+ridgeLine (TopRidge p1 p2)  = mkLine (p1 ^. _ridgePointPos) (p2 ^. _ridgePointPos)
+ridgeLine (SideRidge p1 p2) = mkLine (p1 ^. _ridgePointPos) (p2 ^. _gutterPointPos)
