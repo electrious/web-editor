151f450852775219dad18021619838d07e69fbc8
Eric Wong <ericsyw@gmail.com>
2020-12-30 11:10:08 +0800

    Add RidgeEditorState and RidgeOp data types to manage ridge points and ridges

--- src/housebuilder/RidgeBuilder.purs
+++ src/housebuilder/RidgeBuilder.purs
@@ -4,10 +4,14 @@ module HouseBuilder.RidgeBuilder where
 import Prelude
 
 import Control.Plus (empty)
-import Data.Default (def)
-import Data.Lens (view, (.~), (^.))
+import Data.Default (class Default, def)
+import Data.Lens (Lens', view, (%~), (.~), (^.))
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Record (prop)
+import Data.List (List(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
+import Data.Symbol (SProxy(..))
 import Editor.Common.Lenses (_face, _mouseMove, _name, _point, _position)
 import Editor.ObjectAdder (createObjectAdder, mkCandidatePoint)
 import Editor.SceneEvent (SceneMouseMoveEvent)
@@ -16,6 +20,8 @@ import FRP.Event (Event)
 import FRP.Event.Extra (performEvent)
 import Model.ActiveMode (ActiveMode)
 import Model.HouseBuilder.FloorPlan (FloorPlan)
+import Model.HouseBuilder.House (_ridges)
+import Model.HouseBuilder.Ridge (Ridge, topRidge)
 import Model.HouseEditor.HousePoint (RidgePoint, ridgePoint)
 import Rendering.Node (Node, getParent, node)
 import Three.Core.Face3 (normal)
@@ -23,6 +29,7 @@ import Three.Core.Object3D (worldToLocal)
 import Three.Math.Vector (mkVec3)
 
 
+-- | let user add new ridge point
 addRidgePoint :: forall e. RidgeEditorConf -> Dynamic Boolean -> Node e (Event RidgePoint)
 addRidgePoint cfg canShowDyn = do
     parent <- getParent
@@ -41,6 +48,38 @@ addRidgePoint cfg canShowDyn = do
                   # _position .~ pure (mkVec3 0.0 0.0 0.1)
                   
     map toRidgePoint <$> node opt (createObjectAdder candPntDyn canShowDyn)
+
+
+newtype RidgeEditorState = RidgeEditorState {
+    ridgePoints :: List RidgePoint,
+    ridges      :: List Ridge,
+    tempRidge   :: Maybe Ridge
+    }
+
+derive instance newtypeRidgeEditorState :: Newtype RidgeEditorState _
+instance defaultRidgeEditorState :: Default RidgeEditorState where
+    def = RidgeEditorState {
+        ridgePoints : Nil,
+        ridges      : Nil,
+        tempRidge   : Nothing
+        }
+
+_ridgePoints :: forall t a r. Newtype t { ridgePoints :: a | r } => Lens' t a
+_ridgePoints = _Newtype <<< prop (SProxy :: SProxy "ridgePoints")
+
+_tempRidge :: forall t a r. Newtype t { tempRidge :: a | r } => Lens' t a
+_tempRidge = _Newtype <<< prop (SProxy :: SProxy "tempRidge")
+
+data RidgeOp = ROAddRidgePoint RidgePoint
+             | RODragRidgePoint RidgePoint RidgePoint
+             | ROAddRidge Ridge
+
+
+applyOp :: RidgeOp -> RidgeEditorState -> RidgeEditorState
+applyOp (ROAddRidgePoint p)     st = st # _ridgePoints %~ Cons p
+applyOp (RODragRidgePoint p np) st = st # _tempRidge .~ Just (topRidge p np)
+applyOp (ROAddRidge r)          st = st # _ridges %~ Cons r
+                                        # _tempRidge .~ Nothing
 
 
 newtype RidgeEditorConf = RidgeEditorConf {
