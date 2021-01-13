c4ed9385392e40a7a35f72492bcc53afc26aa8bc
Eric Wong <ericsyw@gmail.com>
2021-01-13 16:45:08 +0800

    Make ActiveMode a HeytingAlgebra so it can be easier to use

--- src/models/ActiveMode.purs
+++ src/models/ActiveMode.purs
@@ -11,9 +11,30 @@ data ActiveMode = Active
 derive instance genericActiveMode :: Generic ActiveMode _
 derive instance eqActiveMode :: Eq ActiveMode
 derive instance ordActiveMode :: Ord ActiveMode
-
 instance showActiveMode :: Show ActiveMode where
     show = genericShow
+instance heytingActiveMode :: HeytingAlgebra ActiveMode where
+    tt = Active
+    ff = Inactive
+    implies a b = not a || b
+    
+    conj Active Active = Active
+    conj _ Inactive    = Inactive
+    conj Inactive _    = Inactive
+
+    disj Active _          = Active
+    disj _ Active          = Active
+    disj Inactive Inactive = Inactive
+
+    not Active   = Inactive
+    not Inactive = Active
 
 isActive :: ActiveMode -> Boolean
 isActive = (==) Active
+
+toBoolean :: ActiveMode -> Boolean
+toBoolean = isActive
+
+fromBoolean :: Boolean -> ActiveMode
+fromBoolean true  = Active
+fromBoolean false = Inactive
