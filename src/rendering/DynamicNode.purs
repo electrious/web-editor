9dad31998b6bac00015ec6b984edf7755118a850
Eric Wong <ericsyw@gmail.com>
2021-01-18 14:47:20 +0800

    Fix bug in eventNode and dynamic function that won't dispose child nodes correctly

--- src/rendering/DynamicNode.purs
+++ src/rendering/DynamicNode.purs
@@ -5,15 +5,15 @@ import Prelude
 
 import Control.Monad.Reader (ask)
 import Control.Monad.Writer (tell)
+import Data.Foldable (traverse_)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (fst, snd)
 import Editor.Disposable (Disposee(..), dispose)
-import Effect (Effect)
 import Effect.Class (liftEffect)
-import FRP.Dynamic (Dynamic, performDynamic, subscribeDyn, withLast)
+import FRP.Dynamic (Dynamic, current, performDynamic, step, subscribeDyn, withLast)
 import FRP.Event (Event, subscribe)
 import FRP.Event as Evt
-import FRP.Event.Extra (performEvent)
+import FRP.Event.Extra (multicast, performEvent)
 import Rendering.Node (Node, runNode)
 import Rendering.NodeRenderable (class NodeRenderable, render)
 
@@ -22,22 +22,25 @@ import Rendering.NodeRenderable (class NodeRenderable, render)
 getLast :: forall a r. { last :: a | r } -> a
 getLast o = o.last
 
-toDispose :: Maybe Disposee -> Effect Unit
-toDispose Nothing  = pure unit
-toDispose (Just d) = dispose d
-
 -- | run an event Node action in a Node context
 eventNode :: forall e a. Event (Node e a) -> Node e (Event a)
 eventNode evt = do
     env <- ask
 
     let rEvt = performEvent $ flip runNode env <$> evt
-        resEvt = fst <$> rEvt
+        
+        dispEvt = multicast $ snd <$> rEvt
 
-    d <- liftEffect $ subscribe (Evt.withLast $ snd <$> rEvt) (getLast >>> toDispose)
-    tell $ Disposee d
+        -- store the Disposee object for the current node action
+        dispDyn = step Nothing $ Just <$> dispEvt
+        -- function to dispose the current node action
+        toDisposeCurVal = current dispDyn >>= traverse_ dispose
+        
+    d <- liftEffect $ subscribe (Evt.withLast dispEvt) (getLast >>> traverse_ dispose)
+    
+    tell $ Disposee $ d *> toDisposeCurVal
 
-    pure resEvt
+    pure $ fst <$> rEvt
 
 -- | run an event node action in a node context and omit the result
 eventNode_ :: forall e a. Event (Node e a) -> Node e Unit
@@ -52,12 +55,15 @@ dynamic :: forall e a. Dynamic (Node e a) -> Node e (Dynamic a)
 dynamic dyn = do
     env <- ask
 
-    let rDyn       = performDynamic $ flip runNode env <$> dyn
-        resDyn     = fst <$> rDyn
-    d <- liftEffect $ subscribeDyn (withLast $ snd <$> rDyn) (getLast >>> toDispose)
-    tell $ Disposee d
+    let rDyn    = performDynamic $ flip runNode env <$> dyn
+        dispDyn = snd <$> rDyn
+
+        toDisposeCurVal = current dispDyn >>= dispose
+
+    d <- liftEffect $ subscribeDyn (withLast dispDyn) (getLast >>> traverse_ dispose)
+    tell $ Disposee $ d *> toDisposeCurVal
     
-    pure resDyn
+    pure $ fst <$> rDyn
 
 -- | run a dynamic node action in a node context and omit the result
 dynamic_ :: forall e a. Dynamic (Node e a) -> Node e Unit
