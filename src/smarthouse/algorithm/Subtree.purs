b3087cd7748c488e2556b3adc37711d6099e3d8f
Eric Wong <ericsyw@gmail.com>
2021-03-30 15:56:57 +0800

    convert Subtree to a list of lines

--- src/smarthouse/algorithm/Subtree.purs
+++ src/smarthouse/algorithm/Subtree.purs
@@ -1,10 +1,16 @@
 module Smarthouse.Algorithm.Subtree where
 
+import Prelude
+
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
+import Data.Lens (Lens', (^.))
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Record (prop)
 import Data.List (List)
 import Data.Newtype (class Newtype)
-import Data.Show (class Show)
+import Data.Symbol (SProxy(..))
+import Math.Line (Line, line)
 import Three.Math.Vector (Vector3)
 
 newtype Subtree = Subtree {
@@ -18,9 +24,20 @@ derive instance genericSubtree :: Generic Subtree _
 instance showSubtree :: Show Subtree where
     show = genericShow
 
+_source :: forall t a r. Newtype t { source :: a | r } => Lens' t a
+_source = _Newtype <<< prop (SProxy :: SProxy "source")
+
+_sinks :: forall t a r. Newtype t { sinks :: a | r } => Lens' t a
+_sinks = _Newtype <<< prop (SProxy :: SProxy "sinks")
+
 subtree :: Vector3 -> Number -> List Vector3 -> Subtree
 subtree source h ss = Subtree {
     source : source,
     height : h,
     sinks  : ss
     }
+
+
+treeLines :: Subtree -> List (Line Vector3)
+treeLines t = line s <$> t ^. _sinks
+    where s = t ^. _source
