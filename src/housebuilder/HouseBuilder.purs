d2e909cd5fa49e2af6f7e8a23b305e58bf19d1ed
Eric Wong <eric.wong@clean.tech>
2021-04-12 10:48:25 +0000

    Add House model and create House from floor polygon and render it

--- src/housebuilder/HouseBuilder.purs
+++ src/housebuilder/HouseBuilder.purs
@@ -4,32 +4,23 @@ import Prelude hiding (degree)
 
 import Custom.Mesh (TapMouseMesh)
 import Data.Default (class Default, def)
-import Data.Foldable (traverse_)
 import Data.Lens (view, (.~), (^.))
-import Data.List (List, singleton)
-import Data.Meter (Meter, meter, meterVal)
 import Data.Newtype (class Newtype)
-import Editor.Common.Lenses (_leadId, _mouseMove, _name, _position)
+import Editor.Common.Lenses (_leadId, _mouseMove, _name)
 import Editor.Editor (Editor)
 import Effect (Effect)
 import Effect.Class (liftEffect)
-import Effect.Class.Console (log)
-import Effect.Random (randomRange)
-import FRP.Event (subscribe)
+import FRP.Event (Event)
 import FRP.Event.Extra (performEvent)
-import HouseBuilder.PolyGeometry (mkPolyGeometry)
 import Math.Angle (degree)
-import Model.Polygon (Polygon, _polyVerts, counterClockPoly)
-import Rendering.DynamicNode (eventNode_)
-import Rendering.Node (Node, getEnv, localEnv, mesh, mkNodeEnv, node, runNode, tapMouseMesh)
+import Model.SmartHouse.House (createHouseFrom)
+import Rendering.DynamicNode (renderEvent)
+import Rendering.Node (Node, getEnv, localEnv, mkNodeEnv, node, runNode, tapMouseMesh)
 import Rendering.TextureLoader (loadTextureFromUrl)
-import SmartHouse.Algorithm.Skeleton (skeletonize)
 import SmartHouse.HouseTracer (traceHouse)
-import Three.Core.Geometry (_bevelEnabled, _depth, mkExtrudeGeometry, mkPlaneGeometry, mkShape)
-import Three.Core.Material (MeshBasicMaterial, mkMeshBasicMaterialWithColor, mkMeshBasicMaterialWithTexture, mkMeshPhongMaterial)
+import Three.Core.Geometry (mkPlaneGeometry)
+import Three.Core.Material (mkMeshBasicMaterialWithTexture)
 import Three.Loader.TextureLoader (clampToEdgeWrapping, repeatWrapping, setRepeat, setWrapS, setWrapT)
-import Three.Math.Color (Color, mkColorRGB)
-import Three.Math.Vector (Vector3, mkVec3, toVec2)
 
 newtype HouseBuilderConfig = HouseBuilderConfig {
     leadId :: Int
@@ -66,52 +57,14 @@ createHouseBuilder = node (def # _name .~ "house-builder") do
 
     let cfg = def # _mouseMove .~ helper ^. _mouseMove
 
-        h = meter 3.5
         --bgTapEvt = const unit <$> helper ^. _tapped
     
-    floorPlanEvt <- localEnv (const cfg) $ traceHouse
-
-    _ <- liftEffect $ subscribe floorPlanEvt (show >>> log)
-
-    -- calculate skeletons
-    let polysEvt = performEvent $ skeletonize (degree 30.0) <<< singleton <<< counterClockPoly <$> floorPlanEvt
-        pos = mkVec3 0.0 0.0 (meterVal h)
-    node (def # _position .~ pure pos) $ eventNode_ $ renderRoofPolys <$> polysEvt
-
-    -- render walls1
-    eventNode_ $ renderWalls h <$> floorPlanEvt
+    floorPlanEvt <- localEnv (const cfg) traceHouse
+    let houseEvt = performEvent $ createHouseFrom (degree 30.0) <$> floorPlanEvt
+    _ :: Event Unit <- renderEvent houseEvt
+    pure unit
 
 
 -- | external API to build a 3D house for 2D lead
 buildHouse :: Editor -> HouseBuilderConfig -> Effect Unit
 buildHouse editor cfg = void $ runNode createHouseBuilder $ mkNodeEnv editor cfg
-
-
-renderRoofPolys :: forall e. List (Polygon Vector3) -> Node e Unit
-renderRoofPolys = traverse_ renderRoofPoly
-
-randomColor :: Effect Color
-randomColor = do
-    r <- randomRange 0.0 1.0
-    g <- randomRange 0.0 1.0
-    b <- randomRange 0.0 1.0
-    mkColorRGB r g b
-
-randomRoofMaterial :: Effect MeshBasicMaterial
-randomRoofMaterial = randomColor >>= mkMeshBasicMaterialWithColor
-
-renderRoofPoly :: forall e. Polygon Vector3 -> Node e Unit
-renderRoofPoly poly = do
-    geo <- liftEffect $ mkPolyGeometry poly
-    mat <- liftEffect randomRoofMaterial
-    void $ mesh (def # _name .~ "roof") geo mat
-
-
-renderWalls :: forall e. Meter -> Polygon Vector3 -> Node e Unit
-renderWalls height poly = do
-    shp <- liftEffect $ mkShape $ (toVec2 <$> poly) ^. _polyVerts
-    geo <- liftEffect $ mkExtrudeGeometry shp $ def # _depth .~ meterVal height
-                                                    # _bevelEnabled .~ false
-    mat <- liftEffect $ mkMeshPhongMaterial 0x999999
-
-    void $ mesh (def # _name .~ "walls") geo mat
---
+++ src/models/smarthouse/House.purs
@@ -0,0 +1,89 @@
+module Model.SmartHouse.House where
+
+import Prelude
+
+import Data.Default (def)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.Lens ((.~), (^.))
+import Data.List (List, singleton)
+import Data.Meter (Meter, meter, meterVal)
+import Data.Newtype (class Newtype)
+import Data.Traversable (traverse_)
+import Data.UUID (UUID, genUUID)
+import Editor.Common.Lenses (_floor, _height, _id, _name, _position, _roofs)
+import Effect (Effect)
+import Effect.Class (liftEffect)
+import Effect.Random (randomRange)
+import HouseBuilder.PolyGeometry (mkPolyGeometry)
+import Math.Angle (Angle)
+import Model.Polygon (Polygon, _polyVerts, counterClockPoly)
+import Model.UUID (class HasUUID)
+import Rendering.Node (Node, mesh, node)
+import Rendering.NodeRenderable (class NodeRenderable)
+import SmartHouse.Algorithm.Skeleton (skeletonize)
+import Three.Core.Geometry (_bevelEnabled, _depth, mkExtrudeGeometry, mkShape)
+import Three.Core.Material (MeshBasicMaterial, mkMeshBasicMaterialWithColor, mkMeshPhongMaterial)
+import Three.Math.Color (Color, mkColorRGB)
+import Three.Math.Vector (Vector3, mkVec3, toVec2)
+
+newtype House = House {
+    id     :: UUID,
+    floor  :: Polygon Vector3,
+    height :: Meter,
+    roofs  :: List (Polygon Vector3)
+    }
+
+derive instance newtypeHouse :: Newtype House _
+derive instance genericHouse :: Generic House _
+instance showHouse :: Show House where
+    show = genericShow
+instance hasUUIDHouse :: HasUUID House where
+    idLens = _id
+
+createHouseFrom :: Angle -> Polygon Vector3 -> Effect House
+createHouseFrom slope poly = do
+    i <- genUUID
+    roofs <- skeletonize slope $ singleton $ counterClockPoly poly
+    pure $ House {
+        id     : i,
+        floor  : poly,
+        height : meter 3.5,   -- default height
+        roofs  : roofs
+        }
+
+
+-- rendering
+instance nodeRenderableHouse :: NodeRenderable e House Unit where
+    render house = do
+        let h = house ^. _height
+            p = mkVec3 0.0 0.0 (meterVal h)
+        renderWalls h $ house ^. _floor
+        node (def # _position .~ pure p) $ traverse_ renderRoofPoly $ house ^. _roofs
+        
+
+randomColor :: Effect Color
+randomColor = do
+    r <- randomRange 0.0 1.0
+    g <- randomRange 0.0 1.0
+    b <- randomRange 0.0 1.0
+    mkColorRGB r g b
+
+randomRoofMaterial :: Effect MeshBasicMaterial
+randomRoofMaterial = randomColor >>= mkMeshBasicMaterialWithColor
+
+renderRoofPoly :: forall e. Polygon Vector3 -> Node e Unit
+renderRoofPoly poly = do
+    geo <- liftEffect $ mkPolyGeometry poly
+    mat <- liftEffect randomRoofMaterial
+    void $ mesh (def # _name .~ "roof") geo mat
+
+
+renderWalls :: forall e. Meter -> Polygon Vector3 -> Node e Unit
+renderWalls height poly = do
+    shp <- liftEffect $ mkShape $ (toVec2 <$> poly) ^. _polyVerts
+    geo <- liftEffect $ mkExtrudeGeometry shp $ def # _depth .~ meterVal height
+                                                    # _bevelEnabled .~ false
+    mat <- liftEffect $ mkMeshPhongMaterial 0x999999
+
+    void $ mesh (def # _name .~ "walls") geo mat
