20db185e76d6265191b0ee90430e368bed45edc8
Eric Wong <ericsyw@gmail.com>
2021-04-21 14:12:16 +0800

    Add subtree info into Roof

--- src/models/smarthouse/Roof.purs
+++ src/models/smarthouse/Roof.purs
@@ -5,26 +5,32 @@ import Prelude
 import Data.Default (def)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.Lens ((.~), (^.))
+import Data.Lens (Lens', (.~), (^.))
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Record (prop)
+import Data.List (List, length)
 import Data.Newtype (class Newtype)
+import Data.Symbol (SProxy(..))
 import Data.Traversable (traverse_)
 import Data.UUID (UUID, genUUID)
 import Editor.Common.Lenses (_id, _name, _polygon, _tapped)
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import FRP.Event (Event)
-import SmartHouse.PolyGeometry (mkPolyGeometryWithUV)
 import Model.Polygon (Polygon, polyOutline)
 import Model.SmartHouse.HouseTextureInfo (HouseTextureInfo, _size, _texture)
 import Model.UUID (class HasUUID, idLens)
 import Rendering.Node (Node, getEnv, tapMesh)
 import SmartHouse.HouseTracer (renderLine)
+import SmartHouse.PolyGeometry (mkPolyGeometryWithUV)
+import Smarthouse.Algorithm.Subtree (Subtree)
 import Three.Core.Material (mkMeshBasicMaterialWithTexture)
 import Three.Math.Vector (Vector3)
 
 newtype Roof = Roof {
-    id      :: UUID,
-    polygon :: Polygon Vector3
+    id       :: UUID,
+    polygon  :: Polygon Vector3,
+    subtrees :: List Subtree
     }
 
 derive instance newtypeRoof :: Newtype Roof _
@@ -34,10 +40,16 @@ instance showRoof :: Show Roof where
 instance hasUUIDRoof :: HasUUID Roof where
     idLens = _id
 
-createRoofFrom :: Polygon Vector3 -> Effect Roof
-createRoofFrom p = do
+_subtrees :: forall t a r. Newtype t { subtrees :: a | r } => Lens' t a
+_subtrees = _Newtype <<< prop (SProxy :: SProxy "subtrees")
+
+createRoofFrom :: Polygon Vector3 -> List Subtree -> Effect Roof
+createRoofFrom p ts = do
     i <- genUUID
-    pure $ Roof { id : i, polygon : p }
+    pure $ Roof { id : i, polygon : p, subtrees : ts }
+
+canBeGable :: Roof -> Boolean
+canBeGable r = length (r ^. _subtrees) < 2
 
 renderRoof :: Roof -> Node HouseTextureInfo (Event UUID)
 renderRoof roof = do
--- src/smarthouse/algorithm/Subtree.purs
+++ src/smarthouse/algorithm/Subtree.purs
@@ -2,18 +2,15 @@ module Smarthouse.Algorithm.Subtree where
 
 import Prelude
 
-import Data.Array (elem, foldl)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Lens (Lens', (^.))
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.List (List(..), (!!))
-import Data.Maybe (Maybe(..))
+import Data.List (List)
 import Data.Newtype (class Newtype)
 import Data.Symbol (SProxy(..))
-import Data.Tuple (Tuple(..))
-import Math.LineSeg (LineSeg, mkLineSeg)
+import Math.LineSeg (LineSeg(..), mkLineSeg)
 import Three.Math.Vector (Vector3)
 
 newtype Subtree = Subtree {
@@ -47,12 +44,5 @@ treeLines :: Subtree -> List (LineSeg Vector3)
 treeLines t = mkLineSeg s <$> t ^. _sinks
     where s = t ^. _source
 
--- get possible gable roof edge
-gableEdge :: Subtree -> List Vector3 -> Maybe (Tuple Vector3 Vector3)
-gableEdge t vs = toEdge $ foldl f (Tuple 0 Nil) $ t ^. _sinks
-    where f (Tuple n ls) v = if elem v vs
-                             then Tuple (n + 1) (Cons v ls)
-                             else Tuple n ls
-          toEdge (Tuple n ls) = if n >= 2
-                                then Tuple <$> ls !! 0 <*> ls !! 1
-                                else Nothing
+gableSubtree :: Subtree -> List (LineSeg Vector3) -> Subtree
+gableSubtree t es = 
