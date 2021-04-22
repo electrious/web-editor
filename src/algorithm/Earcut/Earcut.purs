d21e583bb2907812bb632886be14be1af918d03a
Eric Wong <ericsyw@gmail.com>
2021-04-22 21:15:33 +0800

    Don't use earcut for 3 vertices polygon triangulation and generate the Face3 directly

--- src/algorithm/Earcut/Earcut.purs
+++ src/algorithm/Earcut/Earcut.purs
@@ -2,16 +2,19 @@ module Algorithm.Earcut (triangulatePoly) where
 
 import Prelude
 
-import Data.Array (concatMap)
+import Data.Array (concatMap, length)
 import Data.Lens ((^.))
+import Effect.Unsafe (unsafePerformEffect)
 import Model.Polygon (Polygon, _polyVerts)
 import Model.Roof.RoofPlate (vecArr)
-import Three.Core.Face3 (Face3)
+import Three.Core.Face3 (Face3, mkFace3)
 import Three.Math.Vector (class Vector, getVector)
 
 -- wrapper for the Earcut polygon triangulation algorithm in JS
 foreign import earcut :: Array Number -> Array Face3
 
 triangulatePoly :: forall v. Vector v => Polygon v -> Array Face3
-triangulatePoly p = earcut $ concatMap f $ p ^. _polyVerts
-    where f = vecArr <<< getVector
+triangulatePoly p = if length vs > 3
+                    then earcut $ concatMap (vecArr <<< getVector) vs
+                    else [unsafePerformEffect $ mkFace3 0 1 2]
+    where vs = p ^. _polyVerts
