d6b41a414776b3cddc617968e99d1539e46d3e8b
Eric Wong <ericsyw@gmail.com>
2021-07-01 16:02:07 +0800

    Add code to build the default tree

--- src/models/smarthouse/Tree.purs
+++ src/models/smarthouse/Tree.purs
@@ -2,14 +2,16 @@ module Model.SmartHouse.Tree where
 
 import Prelude
 
-import Data.Lens (Lens')
+import Data.Default (class Default, def)
+import Data.Lens (Lens', (.~))
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.Meter (Meter)
+import Data.Meter (Meter, meter)
 import Data.Newtype (class Newtype)
 import Data.Symbol (SProxy(..))
-import Data.UUID (UUID)
-import Editor.Common.Lenses (_id)
+import Data.UUID (UUID, emptyUUID, genUUID)
+import Editor.Common.Lenses (_height, _id, _position)
+import Effect (Effect)
 import Model.UUID (class HasUUID)
 import Three.Math.Vector (Vector3)
 
@@ -20,6 +22,11 @@ newtype TreePart = TreePart {
 
 derive instance newtypeTreepart :: Newtype TreePart _
 derive instance eqTreePart :: Eq TreePart
+instance defaultTreePart :: Default TreePart where
+    def = TreePart {
+        height : def,
+        dia    : def
+        }
 
 _dia :: forall t a r. Newtype t { dia :: a | r } => Lens' t a
 _dia = _Newtype <<< prop (SProxy :: SProxy "dia")
@@ -39,6 +46,19 @@ newtype Tree = Tree {
 derive instance newtypeTree :: Newtype Tree _
 instance hasUUIDTree :: HasUUID Tree where
     idLens = _id
+instance defaultTree :: Default Tree where
+    def = Tree {
+        id       : emptyUUID,
+        height   : meter 10.0,
+        crown    : def # _height .~ meter 8.0
+                       # _dia    .~ meter 1.0,
+        barrel   : def # _height .~ meter 5.0
+                       # _dia .~ meter 2.0,
+        canopy   : def # _height .~ meter 3.0
+                       # _dia .~ meter 1.0,
+
+        position : def
+        }
 
 _crown :: forall t a r. Newtype t { crown :: a | r } => Lens' t a
 _crown = _Newtype <<< prop (SProxy :: SProxy "crown")
@@ -48,3 +68,10 @@ _barrel = _Newtype <<< prop (SProxy :: SProxy "barrel")
 
 _canopy :: forall t a r. Newtype t { canopy :: a | r } => Lens' t a
 _canopy = _Newtype <<< prop (SProxy :: SProxy "canopy")
+
+
+mkTree :: Vector3 -> Effect Tree
+mkTree p = do
+    i <- genUUID
+    pure $ def # _id .~ i
+               # _position .~ p
